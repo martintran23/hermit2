306ec254f234bb23967fbcfdbd39e8a93dbb6505
Martin Tran <23martintran@gmai.com>
2025-04-16 16:10:30 -0700

    Initial api request

--- Hermit/main.py
+++ Hermit/main.py
@@ -1,1 +1,18 @@
-print('Hello World')
+import requests
+
+url = "https://api.lodgify.com/v2/properties"
+
+api_key = '4PE72MIUggANayatgtL7crI9SyEMllqE25DERi/2+Ue/GnmRXyMjqnsyc61u/frt'
+
+headers = {
+    "X-ApiKey": api_key
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    # 200 means data request successful
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}, {response.text}")
