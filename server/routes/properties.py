29cf79c132b8254e0efc7bf0d921d60ccd7f6b80
Martin Tran <23martintran@gmai.com>
2025-04-29 21:48:16 -0700

    removed properties comments

--- server/routes/properties.py
+++ server/routes/properties.py
@@ -18,57 +18,6 @@ HEADERS = {
 
 # === Search Rentals ===
 @properties_bp.route('/api/properties', methods=['GET'])
-#def list_properties():
-#    location = request.args.get('location')
-#    limit = int(request.args.get('limit', 5))
-#    offset = int(request.args.get('offset', 0))
-
-#    if not location:
-#        return jsonify({"error": "Location parameter is required."}), 400
-
-    # First try to find from MongoDB
-#    cached_listings = list(rentals_collection.find({"search_location": location}).limit(limit))
-
-#    if cached_listings:
-        # If Found in MongoDB, return cached data
-#        for rental in cached_listings:
-#            rental['_id'] = str(rental['_id'])  # MongoDB ID to string for JSON
-#        return jsonify(cached_listings), 200
-
-    # If Not found in MongoDB, fetch from RapidAPI
-#    params = {
-#        "location": location,
-#        "limit": limit,
-#        "offset": offset,
-#        "days_on": "30"
-#    }
-
-#    try:
-#        response = requests.get(RENT_URL, headers=HEADERS, params=params)
-#        response.raise_for_status()
-#        data = response.json()  # check to
-#        print(data.keys())      # see what keys are returned from data
-#        listings = response.json().get('listings', [])
-
-#        if listings:
-            # Save new listings into MongoDB
-#            for listing in listings:
-#                listing['search_location'] = location  # save search keyword for filtering later
-#                rentals_collection.update_one(
-#                    {"property_id": listing['property_id']},
-#                    {"$set": listing},
-#                    upsert=True
-#                )
-
-#        return jsonify(listings), 200
-
-#    except requests.RequestException as e:
-#        if e.response is not None:
-#            print("RapidAPI error:", e.response.text)
-#            return jsonify({"error": e.response.text}), e.response.status_code
-#        else:
-#            print("Request failed without response:", str(e))
-#            return jsonify({"error": str(e)}), 500
 def list_properties():
     location = request.args.get('location')
     try:
